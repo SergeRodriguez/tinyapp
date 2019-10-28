d89a1573be6839b1e00f5dec3b92f8913d1462fb
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-28 19:21:44 +0000

    added a views folder to the directory and added a template in the urls_index.ejs in order to display the object sent from express_server.js

--- express_server.js
+++ express_server.js
@@ -28,8 +28,13 @@ app.get("/hello", (req, res) => {
 app.get("/set", (req, res) => {
   const a = 1;
   res.send(`a = ${a}`);
- });
- 
- app.get("/fetch", (req, res) => {
+});
+
+app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
- });
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});

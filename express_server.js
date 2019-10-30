7989b1b15b1a59fea5323fdadfee2e8c7ed217d4
SergeRodriguez <serge.rodriguez@mail.mcgill.ca>
2019-10-30 00:05:05 +0000

    created a new branch feature/cookies

--- express_server.js
+++ express_server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+var cookieParser = require('cookie-parser');
+app.use(cookieParser());
 app.set("view engine", "ejs");
 
 const urlDatabase = {
@@ -37,16 +38,24 @@ app.get("/fetch", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"]
+   };
+   console.log("this cookie in urls",req.cookies)           
+
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies["username"],
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], username: req.cookies["username"] };
   res.render("urls_show", templateVars)
 })
 
@@ -69,8 +78,22 @@ app.post("/urls/:shortURL/delete", (req, res) =>{
 
 app.post("/urls/:shortURL/edit", (req, res) =>{
   urlDatabase[req.params.shortURL] = req.body.longURL; 
+  console.log(req.body, req.header)
   res.redirect("/urls")
 })
+
+app.post("/login", (req,res)=>{
+  res.cookie("username",req.body.username); 
+  console.log("this cookie",req.cookies)
+  console.log("this res", res)   
+  console.log("this req", req)            
+  res.redirect("/urls")
+});
+
+app.post("/logout", (req,res)=>{
+  res.clearCookie("username")         
+  res.redirect("/urls")
+});
 
 
 
